1e8837b0f1b5839a6a59ebbc0db26dc7a8976bc4
CKGHarju <chrharju@gmail.com>
2018-07-06 20:45:33 +0200

    feat: add some structure

---
+++ src/containers/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        // Navbar
+        // Content
+          //List
+            //Popular
+            //New
+          //Grid
+            //Stream
+          //Chat
+            //MessageList
+            //SendButton
+      </div>
+    );
+  }
+}
+
+export default App;
