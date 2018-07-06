69595f4e8d330a43bb680ad7baaf8269c73baf91
edgarcheverier <edgarcheverier@gmail.com>
2018-07-06 20:56:14 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/AppApp';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import api from './middleware/api';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(api())
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App/>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
