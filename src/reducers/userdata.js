f8c44954466226e159b6b649861ed2edf5e76e57
CKGHarju <chrharju@gmail.com>
2018-07-08 14:51:21 +0200

    feat: removes removed grids from state

--- src/reducers/userdata.js
+++ src/reducers/userdata.js
@@ -2,6 +2,8 @@ const userdata = (state = {}, action) => {
   switch(action.type) {
     case 'USER_DATA':
       return action.userdata;
+    case 'USER_DATA_REFRESH':
+      return {name: state.name, id: state.id, grids: action.grids}
     default: 
       return state;
   }
